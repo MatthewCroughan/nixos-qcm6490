5852936db6f26bac90c41c57f008ccf5f6a36d51
matthewcroughan <matt@croughan.sh>
2025-03-30 23:49:21 +0100

    u-boot works, and add some weird reserved-memory

--- uboot-qcm6490.nix
+++ uboot-qcm6490.nix
@@ -1,10 +1,10 @@
-{ buildUBoot, xxd, bison, flex, openssl, gnutls, fetchFromGitHub, python3Packages }:
+# https://docs.u-boot.org/en/latest/board/qualcomm/board.html
+{ buildUBoot, xxd, bison, flex, openssl, gnutls, fetchgit, python3Packages, android-tools }:
 let
-  qtestsign = fetchFromGitHub {
-    owner = "msm8916-mainline";
-    repo = "qtestsign";
-    rev = "ce6ba20f4ead008e5b522a81179c98dd48ccf06e";
-    hash = "sha256-f4IhfEbv88ui7el2JgpfamdFNJKkTX5EjtA4N2PEEQo=";
+  qtestsign = fetchgit {
+    url = "https://git.codelinaro.org/linaro/qcomlt/u-boot.git";
+    rev = "9f5031424714694721c00c04aa068c3ffc6e7f14";
+    hash = "sha256-scG5Q5WVYlX8rS5TnInkYmXSgO2977qR9zipP1Q0rIY=";
   };
   python = python3Packages.python.withPackages (p: [ p.cryptography ]);
 in
@@ -17,15 +17,17 @@ buildUBoot {
   };
   prePatch = ''
     substituteInPlace configs/qcm6490_defconfig --replace-fail 'qcom/qcs6490-rb3gen2' 'qcom/qcm6490-shift-otter'
+    ln -s ${qtestsign}/qtestsign.py $(pwd)/qtestsign.py
   '';
   extraMakeFlags = [ "DEVICE_TREE=qcom/qcm6490-shift-otter" ];
   defconfig = "qcm6490_defconfig";
   extraMeta.platforms = ["aarch64-linux"];
-  nativeBuildInputs = [ xxd bison flex openssl gnutls ];
+  nativeBuildInputs = [ xxd bison flex openssl gnutls python android-tools ];
 #  BL31 = "${BL31}/bl31.bin";
-  filesToInstall = ["u-boot.elf"];
-  postInstall = ''
-    ${python}/bin/python ${qtestsign}/qtestsign.py -v6 aboot -o $out/u-boot.mbn $out/u-boot.elf
+  filesToInstall = ["u-boot*"];
+  preInstall = ''
+    #${python}/bin/python ${qtestsign}/qtestsign.py -v6 aboot -o $out/u-boot.mbn $out/u-boot.elf
+    ls -lah
   '';
 }
 
